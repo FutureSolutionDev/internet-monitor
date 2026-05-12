2013d269258b997200b1967ee559753de7ebcc1f
Sabry Dawood <kazouya25@gmail.com>
2026-05-12 06:10:39 +0300

    refactor(cmd): add window icon stubs for Windows compatibility
    Add placeholder functions `setWindowIcon` and `setWindowIconFromExe` to the tray stub to support the new icon loading interface on Windows.

--- cmd/gui/tray_stub.go
+++ cmd/gui/tray_stub.go
@@ -10,6 +10,8 @@ import (
 
 func hideWindow(_ uintptr)    {}
 func restoreWindow(_ uintptr) {}
+func setWindowIcon(_ uintptr) {}
+func setWindowIconFromExe(_ uintptr) {}
 
 func initTray(_ webview.WebView, _ uintptr) func() { return func() {} }
 func updateTrayStatus(_ monitor.Status)             {}
